c160d52582bb8c8b3799bafaf02fc6466a5eaaf6
Stephanie Chau <schau@uc.cl>
2019-04-27 16:22:03 -0400

    Modified resolution when merging

--- utils/file_merger.py
+++ utils/file_merger.py
@@ -2,5 +2,5 @@ import os
 from utils.log import progress
 
 def pdf_merger(_img_list: list, _path_name: str) -> None:
-    _img_list[0].save(_path_name, 'PDF', resolution=100,
+    _img_list[0].save(_path_name, 'PDF', resolution=20,
         save_all=True, append_images=progress(_img_list[1:]))
